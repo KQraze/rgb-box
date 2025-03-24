a8a61f40e3a5790b1922888297727fbcce3d1d20
KQraze <pavel.eliseev061@gmail.com>
2025-03-24 15:02:39 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+const div = document.querySelector('div');
+
+
+
+// Черный - rgba(0, 0, 0)
+// Красный - rgba(255, 0, 0)
+// Белый - rgba(255, 255, 255)
+// Синий - rgba(0, 0, 255)
+
+
+onmousemove = (e) => {
+    e.preventDefault();
+
+    const { clientX, clientY } = e;
+    const { left, right, top, bottom, height, width } = div.getBoundingClientRect();
+
+    const x = clientX - left;
+    const y = clientY - top;
+
+    const diagonalK = Math.abs(x / width - y / height);
+
+    const diagonalPercent = (Math.sqrt(x ** 2 + y ** 2) / Math.sqrt(width ** 2 + height ** 2)) * (1 - diagonalK);
+
+    const rgb = {
+        red: 255 * (x / width),
+        green: 255 * (y / height),
+        blue: 255 * diagonalPercent,
+    }
+
+    const clientInside = clientX > left && clientX < right && clientY > top && clientY < bottom;
+
+    if (!clientInside) {
+        rgb.red = 0;
+        rgb.green = 0;
+        rgb.blue = 0;
+    }
+
+    div.style.background = `rgba(${rgb.red}, ${rgb.green}, ${rgb.blue})`;
+    div.innerHTML = `rgba(${~~rgb.red}, ${~~rgb.green}, ${~~rgb.blue})`;
+}
